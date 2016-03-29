b4a562dd28d5af1bb9b5e62d54c5e13324ae7d26
Jonatan Pålsson <jonatan.p@gmail.com>
2016-03-29 14:57:49 +0200

    config: Only use day, month and year for today_fmt

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ copyright = u'2016 Johan Thelin, Jonatan Pålsson & Oscar Andreasson. This work 
 # built documents.
 #
 # The short X.Y version.
-version = '2015-03'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -71,7 +71,7 @@ release = version
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 # Else, today_fmt is used as the format for a strftime call.
-today_fmt = '%B %d, %Y at %H:%M CET'
+today_fmt = '%B %d, %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
